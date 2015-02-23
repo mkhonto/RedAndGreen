3e7928480db2c4d797567139de2daea328dc98dd
mkhonto <jabulani@codex.co>
2015-02-23 12:32:49 +0200

    finish task3

---
+++ task3.js
@@ -0,0 +1,6 @@
+document.getElementsByTagName("button")[0].addEventListener("click", function(){
+  document.getElementsByTagName("div")[0].style.backgroundColor ="green";
+});
+document.getElementsByTagName("button")[1].addEventListener("click", function(){
+  document.getElementsByTagName("div")[1].style.backgroundColor ="red";
+});
